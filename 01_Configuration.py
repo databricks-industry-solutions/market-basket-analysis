868b76503004f02de2dbbda06b412d5202e39a18
dbbnicole <dbbnicole>
2022-08-06 03:51:29 +0000

    Add notebooks

---
+++ 01_Configuration.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+# MAGIC %md This notebook provides access to consistent configuration settings across the notebooks that make up this solution accelerator.  It also provides instructions for the setup of the files required by these notebooks.
+
+# COMMAND ----------
+
+# DBTITLE 1,Initialize Config Settings
+if 'config' not in locals():
+  config = {}
+
+# COMMAND ----------
+
+# MAGIC %md The path of the mount point and the folders containing these files are specified as follows:
+
+# COMMAND ----------
+
+# DBTITLE 1,File Path Configurations
+config['mount_point'] = '/tmp/instacart_market_basket'
+config['orders_files'] = config['mount_point'] + '/bronze/orders'
+config['products_files'] = config['mount_point'] + '/bronze/products'
+config['order_products_files'] = config['mount_point'] + '/bronze/order_products'
+config['departments_files'] = config['mount_point'] + '/bronze/departments'
+config['aisles_files'] = config['mount_point'] + '/bronze/aisles'
+
+# COMMAND ----------
+
+# DBTITLE 1,Set up mlflow experiment in the user's personal workspace folder
+import mlflow
+useremail = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
+experiment_name = f"/Users/{useremail}/market_basket"
+mlflow.set_experiment(experiment_name) 
+
+# COMMAND ----------
+
+# MAGIC %md © 2021 Databricks, Inc. All rights reserved. The source in this notebook is provided subject to the Databricks License. All included or referenced third party libraries are subject to the licenses set forth below.
