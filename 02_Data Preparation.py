8ce12e41dbf4cd825a357f09ef150ec5c92897b4
dbbnicole <dbbnicole>
2022-10-22 04:35:13 +0000

    Add reference to repo and webpage

--- 02_Data Preparation.py
+++ 02_Data Preparation.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md 
+# MAGIC You may find this series of notebooks at https://github.com/databricks-industry-solutions/market-basket-analysis. For more information about this solution accelerator, visit https://www.databricks.com/solutions/accelerators/recommendation-engines.
+
+# COMMAND ----------
+
 # MAGIC %md The purpose of this notebook is to prepare the data with which we will build a market basket recommender.  
 
 # COMMAND ----------
--- 03_Generate Rules.py
+++ 03_Generate Rules.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md 
+# MAGIC You may find this series of notebooks at https://github.com/databricks-industry-solutions/market-basket-analysis. For more information about this solution accelerator, visit https://www.databricks.com/solutions/accelerators/recommendation-engines.
+
+# COMMAND ----------
+
 # MAGIC %md The purpose of this notebook is to generate association rules with which we can make product recommendations. 
 
 # COMMAND ----------
